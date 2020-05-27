975bf1f67e6facadfbdc371d4d513a0a2ba1e247
Lucy Tabaczynsky <ltbczpro@gmail.com>
2020-05-27 12:13:52 +0200

    feature: wip

--- roman_number/src/RomanNumber.php
+++ roman_number/src/RomanNumber.php
@@ -6,6 +6,42 @@ class RomanNumber
 {
     public function convert(int $int): string
     {
-        return "";
+        $romanNumber = "";
+
+        if ($int >= 1 && $int <= 3) {
+            for ($i=0; $i < $int; $i++) { 
+                $romanNumber .= "I";
+            }
+        }
+
+        if ($int == 4) {
+            $romanNumber = "IV";
+        }
+
+        if ($int == 5) {
+            $romanNumber = "V";
+        }
+
+        if ($int >= 6 && $int <= 8) {
+            $romanNumber = "V";
+            for ($i=5; $i < $int; $i++) { 
+                $romanNumber .= "I";
+            }
+        }
+
+        if ($int == 9) {
+            $romanNumber = "IX";
+        }
+
+        /*if ($int >= 10 && $int <= 30) {
+            $modulo = $int%10;
+            if ($modulo == 0) {
+                for ($i=0; $i < $modulo ; $i++) { 
+                    $romanNumber .= "X";
+                }
+            }
+        }*/
+
+        return $romanNumber;
     }
 }
--- roman_number/tests/RomanNumberTest.php
+++ roman_number/tests/RomanNumberTest.php
@@ -9,15 +9,39 @@ require __DIR__.'/../vendor/autoload.php';
 
 class RomanNumberTest extends TestCase
 {
-
-    public function testConvert()
+    /**
+     * @dataProvider convertDataProvider
+     */
+    public function testConvert($arabic, $roman)
     {
-        $amount = RomanNumber::convert(5);
+        $amount = RomanNumber::convert($arabic);
+
+        $romanNumber = ['I', 'V', 'X', 'D', 'L', 'C', 'M'];
 
         $this->assertTrue(is_string($amount));
-        $this->assertRegExp("/[IVXLDCM]/", $amount);
-        //doit revoir le package (nom etc)
-        //doit vérifier que convert() ne sort QUE des chaines avec IVXLDCM
+
+        for ($i = 0; $i < strlen($amount); $i++) { 
+            $this->assertTrue(in_array($amount[$i], $romanNumber), "Character denied");
+        }
+
+        $this->assertEquals($roman, $amount);
     }
 
+    public function convertDataProvider()
+    {
+        return [
+            "1 translates to I" => ["arabic" => 1, "roman" => "I"],
+            "2 translates to I" => ["arabic" => 2, "roman" => "II"],
+            "3 translates to I" => ["arabic" => 3, "roman" => "III"],
+            "4 translates to I" => ["arabic" => 4, "roman" => "IV"],
+            "5 translates to I" => ["arabic" => 5, "roman" => "V"],
+            "6 translates to I" => ["arabic" => 6, "roman" => "VI"],
+            "7 translates to I" => ["arabic" => 7, "roman" => "VII"],
+            "8 translates to I" => ["arabic" => 8, "roman" => "VIII"],
+            "9 translates to I" => ["arabic" => 9, "roman" => "IX"],
+            "10 translates to X" => ["arabic" => 10, "roman" => "X"],
+            "20 translates to XX" => ["arabic" => 20, "roman" => "XX"],
+            "30 translates to XXX" => ["arabic" => 30, "roman" => "XXX"],
+        ];
+    }
 }
